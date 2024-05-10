4e55051a364857513cfa489db90cdda0e83d7b8c
Git-haan <ishaan.reddy5@gmail.com>
2024-05-10 17:16:11 -0400

    first commit

---
+++ src/utils.js
@@ -0,0 +1,29 @@
+export function displayDialouge(text, onDisplayEnd) {
+    const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dialogueUI.style.display = "block";
+
+    let index = 0;
+    let currentText = "";
+    const interval = setInterval(() => {
+        if (index < text.length) {
+            currentText += text[index];
+            dialogue.innerText = currentText;
+            index++;
+            return;
+        }
+
+        clearInterval(interval);
+    }, 5);
+
+    const onCloseBtnClick = () => {
+        onDisplayEnd();
+        dialogueUI.style.display = "none";
+        dialogue.innerText = "";
+        clearInterval(interval);
+        closeBtn.removeEventListener("click", onCloseBtnClick);
+    }
+
+    closeBtn.addEventListener("click", onCloseBtnClick);
+}
