e74742a4e7d61c5261f46905f6ee6b4ef738378a
Git-haan <ishaan.reddy5@gmail.com>
2024-05-11 16:14:38 -0400

    descriptions added

--- src/constants.js
+++ src/constants.js
@@ -5,5 +5,10 @@ export const dialogueData = {
   "cs-degree": `I am currently pursuing a Computer Science degree at Georgia Institue of Technology. My academic interests are diverse, and I strive to continually expand my knowledge base and network with peers who share my passion for technology.`,
   musicbox: `This is my music box. Music has always been an instrumental part of my life. While I indulge in various music genres, I predominantly listen to acoustic pop beats.`,
   bed: `This is the place where I spend a considerable portion of my time. I sleep, read, and watch movies here. Besides, this is where I have some of my most creative ideas and experience bizarre dreams.`,
-  exit: `If you would like to exit the portfolio, please close the tab. But before you leave, you are welcome to check out similar projects on my <a href="https://github.com/Git-haan" target="_blank">GitHub</a>!`,
+  tv: `This is my TV. I enjoy watching movies and TV shows in my free time. I am an avid admirer of the Marvel Cinematic Universe and the Harry Potter saga.`,
+  chest: `This drawer contains a collection of some of my most notable projects. Among them is a Python chat room, which utilizes complex Server Query and Socket protocols. By employing Tkinter as my canvas, I was able to intricately weave together code, enabling seamless real-time communication among clients.`,
+  storage: `These are my storage boxes, housing a robust collection of my technical proficiencies as a software developer. I am proficient in JavaScript and Python, and I am currently learning Typescript, React JS, and Tailwind CSS. I am also familiar with Git and SQL.`,
+  key: `Recently, I have become particularly drawn to the intricate realm of UI development, where I perceive frameworks as the keys unlocking innovative designs. Delving into the seamless integration of Tailwind CSS and React JS, I am impassioned by the craft of UI/UX Development.`,
+  family: `Family is the cornerstone of my life. My family has been my unwavering support system, and I am grateful for their encouragement and guidance. They have instilled in me the values of hard work, perseverance, and integrity.`,
+  exit: `If you would like to exit the portfolio, please close the tab. But before you leave, you are welcome to check out similar projects at Github.com/Git-haan!`,
 };
